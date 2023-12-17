39deca24dfa30427c947b6866b4b60127a4bd478
Prem Methe <premmethe1303@gmail.com>
2023-12-17 11:42:53 +0530

    Migration added

---
+++ backend/migrations/20231217060556-create-stationary-product.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('StationaryProducts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      rating: {
+        type: Sequelize.INTEGER
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('StationaryProducts');
+  }
+};
--- backend/models/dairy.js
+++ backend/models/dairy.js
@@ -1,18 +1,25 @@
-module.exports = (sequelize, Sequelize) => {
-  const DairyProduct = sequelize.define("dairy_products", {
-    name: {
-      type: Sequelize.STRING,
-    },
-    description: {
-      type: Sequelize.STRING,
-    },
-    rating: {
-      type: Sequelize.INTEGER,
-    },
-    price: {
-      type: Sequelize.INTEGER,
-    },
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class DairyProduct extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  DairyProduct.init({
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    rating: DataTypes.INTEGER,
+    price: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'DairyProduct',
   });
-
   return DairyProduct;
 };
